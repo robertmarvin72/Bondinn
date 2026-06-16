958e64a5caf70a8938c4e4f1af83c0261d743af4
Robert <robbinn@gmail.com>
2026-06-16 22:34:23 +0000

    Finances and buy sell sheep

--- world.gd
+++ world.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 enum Weather { SUNNY, RAIN, WIND, STORM }
 
+const SHEEP_BUY_PRICE = 25000
+const SHEEP_SELL_PRICE = 20000
+
 var barn_hay: int = 0
 var barn_capacity: int = 1000
 var day_count: int = 1
@@ -9,6 +12,9 @@ var current_weather: Weather = Weather.SUNNY
 
 var sheep_count: int = 50
 var hay_per_sheep_per_day: float = 0.2
+
+var money: int = 2500000
+var daily_cost: int = 5000
 
 var selected_field = null
 
@@ -19,7 +25,10 @@ var selected_field = null
 @onready var weather_label: Label = $UI/FieldPanel/WeatherLabel
 @onready var next_day_button: Button = $UI/FieldPanel/NextDayButton
 @onready var sheep_label: Label = $UI/FieldPanel/SheepLabel
+@onready var money_label: Label = $UI/FieldPanel/MoneyLabel
 @onready var warning_label: Label = $UI/FieldPanel/WarningLabel
+@onready var buy_sheep_button: Button = $UI/FieldPanel/BuySheepButton
+@onready var sell_sheep_button: Button = $UI/FieldPanel/SellSheepButton
 @onready var camera: Camera3D = $Camera3D
 
 func _weather_name() -> String:
@@ -38,7 +47,11 @@ func _ready():
 	day_label.text = "Dagur: " + str(day_count)
 	weather_label.text = "Veður: " + _weather_name()
 	sheep_label.text = "Kindur: " + str(sheep_count)
+	money_label.text = "Peningar: " + str(money) + " kr."
 	warning_label.text = ""
+	buy_sheep_button.pressed.connect(_on_buy_sheep_pressed)
+	sell_sheep_button.pressed.connect(_on_sell_sheep_pressed)
+	_update_sheep_buttons()
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
@@ -93,15 +106,44 @@ func _on_next_day_pressed():
 	var consumed: int = int(sheep_count * hay_per_sheep_per_day)
 	if barn_hay < consumed:
 		barn_hay = 0
-		warning_label.text = "Viðvörun: Ekki nóg hey fyrir kindurnar!"
 	else:
 		barn_hay -= consumed
-		warning_label.text = ""
+	money -= daily_cost
+	var warnings: Array = []
+	if barn_hay == 0 and consumed > 0:
+		warnings.append("Viðvörun: Ekki nóg hey fyrir kindurnar!")
+	if money < 0:
+		warnings.append("Viðvörun: Búið er í skuld!")
+	warning_label.text = "\n".join(warnings)
 	day_label.text = "Dagur: " + str(day_count)
 	weather_label.text = "Veður: " + _weather_name()
 	hay_label.text = "Hey í hlöðu: " + str(barn_hay) + " / " + str(barn_capacity)
+	money_label.text = "Peningar: " + str(money) + " kr."
+	_update_sheep_buttons()
 	if selected_field != null:
 		_update_panel()
+
+func _update_sheep_buttons() -> void:
+	buy_sheep_button.disabled = money < 10 * SHEEP_BUY_PRICE
+	sell_sheep_button.disabled = sheep_count < 10
+
+func _on_buy_sheep_pressed() -> void:
+	if money < 10 * SHEEP_BUY_PRICE:
+		return
+	money -= 10 * SHEEP_BUY_PRICE
+	sheep_count += 10
+	sheep_label.text = "Kindur: " + str(sheep_count)
+	money_label.text = "Peningar: " + str(money) + " kr."
+	_update_sheep_buttons()
+
+func _on_sell_sheep_pressed() -> void:
+	if sheep_count < 10:
+		return
+	sheep_count -= 10
+	money += 10 * SHEEP_SELL_PRICE
+	sheep_label.text = "Kindur: " + str(sheep_count)
+	money_label.text = "Peningar: " + str(money) + " kr."
+	_update_sheep_buttons()
 
 func _update_panel():
 	field_info.text = selected_field.get_field_info()
